9f741fedc920aed55fb4085e17071f6a3daa15f3
Jadson França <63564173+Jads85@users.noreply.github.com>
2021-09-09 11:47:53 -0300

    Estudo componentes React

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+export default function Card(){
+    return(
+        <div className="card-container">
+            <p>Components:</p>
+            <p>Makes it easier to keep smaller parts working properly</p>
+            <p>You can reuse them, meaning less code to write</p>
+        </div>
+    );
+}
